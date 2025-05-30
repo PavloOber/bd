1326ecbaf2d7020774df77b73e56b6c862c3485b
PavloOberemko <pavlospain8@gmail.com>
2025-05-30 09:46:48 +0200

    2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import cargar_materiales, cargar_usuarios
 from menu import Menu
-#1111
+
 if __name__ == "__main__":
     # Cargar materiales y usuarios al inicio
     materiales = cargar_materiales()
